4a2a02df1d6317896a8f3c4f63918dc20a697baa
Lucas Pluvinage <lucas@tarides.com>
2020-11-09 12:07:37 +0100

    dune cache.

--- src/pipeline.ml
+++ src/pipeline.ml
@@ -7,10 +7,11 @@ let weekly = Current_cache.Schedule.v ~valid_for:(Duration.of_day 7) ()
 
 let targets = ["unix"; "hvt"] (*; "xen"; "virtio"; "spt"; "muen"; "genode"]*)
 let stages = [
-    ("tutorial", ["noop"]);
-    ("tutorial", ["noop-functor"; "hello"; "hello-key"; "app_info"; "lwt"]);
-    ("device-usage", ["block"; "clock"; "conduit_server"; "console"; "http-fetch"; "kv_ro"; "network"; "ping6"; "prng"; "tracing"]);
-    ("applications", ["dhcp"; "dns"; "static_website_tls"])]
+    ("1: test-target", "tutorial", ["noop"]);
+    ("2: tutorial", "tutorial", ["noop-functor"; "hello"; "hello-key"; "app_info"]);
+    ("3: tutorial-lwt", "tutorial", ["lwt/echo_server"; "lwt/heads1"; "lwt/heads2";"lwt/timeout1";"lwt/timeout2"] );
+    ("4: devices", "device-usage", ["block"; "clock"; "conduit_server"; "console"; "http-fetch"; "kv_ro"; "network"; "ping6"; "prng"; "tracing"]);
+    ("5: applications", "applications", ["dhcp"; "dns"; "static_website_tls"])]
 
 
 let ocaml_4_11_1 ~base =
@@ -42,6 +43,7 @@ let mirage_skeleton_dockerfile ~base=
   let open Dockerfile in
   from (Docker.Image.hash base) @@
   env [("OCAMLFIND_CONF", "/home/opam/.opam/4.11.1/lib/findlib.conf")] @@ (* TODO: Dunified findlib is not working correctly.*)
+  run "sh -c \"echo '(lang dune 2.0)\n(cache enabled)\n' >> /home/opam/.config/dune/config\"" @@
   copy ~chown:"opam:opam" ~src:["."] ~dst:"/src/mirage-skeleton" ()
 
 let test_unikernel_dockerfile ~base =
@@ -49,6 +51,7 @@ let test_unikernel_dockerfile ~base =
   from (Docker.Image.hash base) @@
   run "mkdir -p /home/opam/.cache/duniverse/" @@
   volume "/home/opam/.cache/duniverse/" @@
+  volume "/home/opam/.cache/dune/" @@
   copy ~chown:"opam:opam" ~src:["./scripts/unikernel_test.sh"] ~dst:"/" () @@ 
   entrypoint_exec ["/unikernel_test.sh"]
 
@@ -75,7 +78,10 @@ let build_test_image ~repo_mirage_skeleton ~repo_mirage_dev ~repo_mirage_ci () =
   Docker.build ~label:"unikernel tester" ~pull:false ~dockerfile:to_build (`Git src_mirage_ci) (* Step 4: add script. *)
 
 let run_test ~test_image ~unikernel ~target =
-  Docker.run ~label:(unikernel^"-"^target) ~run_args:["-v"; "duniverse-cache:/home/opam/.cache/duniverse"] test_image ~args:["/src/mirage-skeleton/"^unikernel; target]
+  Docker.run ~label:(unikernel^"-"^target) 
+    ~run_args:["-v"; "duniverse-cache:/home/opam/.cache/duniverse"; "-v"; "dune-cache:/home/opam/.cache/dune"] 
+    ~args:["/src/mirage-skeleton/"^unikernel; target]
+    test_image 
 
 let test_target ~test_image ~stage ~unikernels ~target = 
   unikernels
@@ -87,11 +93,11 @@ let v ~repo_mirage_skeleton ~repo_mirage_dev ~repo_mirage_ci () =
   in
   let rec aux test_image = function
     | [] -> Current.ignore_value test_image
-    | (stage, unikernels)::[] -> 
+    | (_, stage, unikernels)::[] -> 
         targets
         |> List.map (fun target -> test_target ~test_image ~stage ~unikernels ~target)
         |> Current.all
-    | (stage, unikernels)::q ->
+    | (_, stage, unikernels)::q ->
       let test_stage =
         targets
         |> List.map (fun target -> test_target ~test_image ~stage ~unikernels ~target)
