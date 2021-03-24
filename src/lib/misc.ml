e4d31ddaad3171606b398fd2690f5af9c20a313f
Lucas Pluvinage <lucas@tarides.com>
2021-03-24 16:30:20 +0100

    fix base image detection

--- src/lib/misc.ml
+++ src/lib/misc.ml
@@ -1,17 +1,16 @@
-let get_base_image package =
-  let deps = Package.universe package |> Package.Universe.deps in
+let base_image_version package =
+  let deps = Package.all_deps package in
   let ocaml_version =
     deps
-    |> List.find_opt (fun pkg -> pkg |> Package.opam |> OpamPackage.name_to_string = "ocaml")
+    |> List.find_opt (fun pkg -> pkg |> Package.opam |> OpamPackage.name_to_string = "ocaml-base-compiler")
     |> Option.map (fun pkg -> pkg |> Package.opam |> OpamPackage.version_to_string)
     |> Option.value ~default:"4.12.0"
   in
-  let base_image_version =
-    match Astring.String.cuts ~sep:"." ocaml_version with
-    | [ major; minor; _micro ] -> major ^ "." ^ minor
-    | _xs -> "4.12"
-  in
-  Spec.make ("ocaml/opam:ubuntu-ocaml-" ^ base_image_version)
+  match Astring.String.cuts ~sep:"." ocaml_version with
+  | [ major; minor; _micro ] -> major ^ "." ^ minor
+  | _xs -> "4.12"
+
+let get_base_image package = Spec.make ("ocaml/opam:ubuntu-ocaml-" ^ base_image_version package)
 
 let network = Voodoo.network
 
@@ -19,7 +18,7 @@ let docs_cache_folder = "/home/opam/docs-cache/"
 
 let cache = [ Obuilder_spec.Cache.v ~target:docs_cache_folder "ci-docs" ]
 
-let rsync_pull ?(digest="") folders =
+let rsync_pull ?(digest = "") folders =
   let sources =
     List.map
       (fun folder -> Fmt.str "%s:%s/./%a" Config.ssh_host Config.storage_folder Fpath.pp folder)
@@ -33,4 +32,5 @@ let rsync_pull ?(digest="") folders =
   | [] -> Obuilder_spec.comment "no sources to pull"
   | _ ->
       Obuilder_spec.run ~secrets:Config.ssh_secrets ~cache ~network
-        "rsync --delete -avzR %s %s  && rsync -aR %s ./ && echo 'pulled: %s'" sources docs_cache_folder cache_sources digest
+        "rsync --delete -avzR %s %s  && rsync -aR %s ./ && echo 'pulled: %s'" sources
+        docs_cache_folder cache_sources digest
