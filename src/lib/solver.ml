fb547696a6b86adc3bf1bc70ba4f103cdafc7f3f
Lucas Pluvinage <lucas@tarides.com>
2021-03-27 13:57:52 +0100

    Solver cache: use multiple files w/ marshal

--- src/lib/solver.ml
+++ src/lib/solver.ml
@@ -96,51 +96,65 @@ module SolverCache = struct
   module Outcome = struct
     type t = Package.t list [@@deriving yojson]
 
-    let marshal t = t |> to_yojson |> Yojson.Safe.to_string
+    (** TODO: too much pressure on the DB *)
+    let marshal t = Marshal.to_string t []
 
-    let unmarshal t = t |> Yojson.Safe.from_string |> of_yojson |> Result.get_ok
+    let unmarshal t = Marshal.from_string t 0
   end
 
-  type cache = (Track.t * Package.t option) list [@@deriving yojson]
+  let fname track =
+    let digest = Track.digest track in
+    let name = Track.pkg track |> OpamPackage.name_to_string in
+    let name_version = Track.pkg track |> OpamPackage.version_to_string in
+    Fpath.(Current.state_dir id / name / name_version / digest)
+
+  let is_in_cache track =
+    let fname = fname track in
+    match Bos.OS.Path.exists fname with Ok true -> true | _ -> false
+
+  type cache_value = Package.t option
+
+  let cache_read track : cache_value option =
+    let fname = fname track in
+    try
+      let file = open_in (Fpath.to_string fname) in
+      Some (Marshal.from_channel file)
+    with Failure _ -> None
+
+  let cache_write ((track, value) : Track.t * cache_value) =
+    let fname = fname track in
+    let open Rresult in
+    let _ = Bos.OS.Dir.create (fst (Fpath.split_base fname)) |> Result.get_ok in
+    let file = open_out (Fpath.to_string fname) in
+    Marshal.to_channel file value [];
+    close_out file
 
   let run No_context job Key.{ packages; blacklist; platform } opam =
     let open Lwt.Syntax in
     let* () = Current.Job.start ~level:Harmless job in
-    let state_file = Fpath.(Current.state_dir id / "solved.json" |> to_string) in
-    let cache_data =
-      try
-        match Yojson.Safe.from_file state_file |> cache_of_yojson with
-        | Ok data -> data |> Track.Map.of_list
-        | Error _ -> Track.Map.empty
-      with Sys_error _ -> Track.Map.empty
-    in
-    let to_do = List.filter (fun x -> not (Track.Map.mem x cache_data)) packages in
+    let to_do = List.filter (fun x -> not (is_in_cache x)) packages in
     let* solved =
       Lwt_list.map_p
         (fun pkg ->
           let+ res = perform_solve ~job ~opam ~platform pkg in
-          (pkg, res))
+          let root = Track.pkg pkg in
+          let result =
+            match res with
+            | Ok (packages, commit) -> Some (Package.make ~blacklist ~commit ~root packages)
+            | Error (`Msg msg) ->
+                Current.Job.log job "Solving failed for %s: %s" (OpamPackage.to_string root) msg;
+                None
+          in
+          cache_write (pkg, result);
+          Option.is_some result)
         to_do
     in
-    let solved_success =
-      List.map
-        (fun (track, result) ->
-          let root = Track.pkg track in
-          track, 
-          match result with
-          | Ok (packages, commit) -> Some (Package.make ~blacklist ~commit ~root packages)
-          | Error (`Msg msg) ->
-              Current.Job.log job "Solving failed for %s: %s" (OpamPackage.to_string root) msg;
-              None)
-        solved
-    in
-    let cache_data =
-      Track.Map.add_seq (List.to_seq solved_success) cache_data |> Track.Map.bindings
-    in
-    Current.Job.log job "Tracked: %d / Solved: %d / New: %d / Success: %d" (List.length packages)
-      (List.length cache_data) (List.length solved) (List.length solved_success);
-    let () = cache_data |> cache_to_yojson |> Yojson.Safe.to_file state_file in
-    Lwt.return_ok (List.filter_map snd cache_data)
+    Current.Job.log job "Solved: %d / New: %d / Success: %d" (List.length packages)
+      (List.length solved)
+      (List.length (solved |> List.filter (fun x -> x)));
+    let data = List.rev_map (fun v -> cache_read v |> Option.get) packages in
+    Current.Job.log job "Loaded data!";
+    Lwt.return_ok (data |> List.filter_map (fun x -> x))
 end
 
 module Solver = Current_cache.Generic (SolverCache)
