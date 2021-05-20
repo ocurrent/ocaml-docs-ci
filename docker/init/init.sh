ed51b8c505f2d17a85de3142800542e1c784ff8b
Lucas Pluvinage <lucas@tarides.com>
2021-05-20 17:03:38 +0200

    don't depend on external dockerfiles

---
+++ docker/init/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+KEYFILE=${KEYFILE:-/ssh/id_ed25519}
+KEYTYPE=${KEYTYPE:-ed25519}
+
+if [ ! -f $KEYFILE ]; then
+    echo "Creating a new keypair in $KEYFILE"
+    ssh-keygen -q -t $KEYTYPE -f $KEYFILE -N '' <<<y
+    cp $KEYFILE.pub /ssh/authorized_keys
+fi
