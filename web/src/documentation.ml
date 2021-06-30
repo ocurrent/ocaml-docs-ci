b270b6c39fcb45bbe8a13e265ffe2225bee05c7c
Lucas Pluvinage <lucas@tarides.com>
2021-06-30 11:12:05 +0200

    vendor graphql-ppx

--- web/src/documentation.ml
+++ web/src/documentation.ml
@@ -17,33 +17,18 @@ end
 module StringMap = OpamStd.String.Map
 
 module Stats = struct
-  type status_count = {
-    total : int;
-    failed : int;
-    pending : int;
-    success : int;
-  }
+  type status_count = { total : int; failed : int; pending : int; success : int }
 
   let to_string { total; failed; pending; success } =
-    let pending =
-      match pending with 0 -> "" | n -> Fmt.str "/ %d 🟠" pending
-    in
+    let pending = match pending with 0 -> "" | n -> Fmt.str "/ %d 🟠" pending in
     let failed = match failed with 0 -> "" | n -> Fmt.str "/ %d ❌" failed in
     Fmt.str "%d ↦ %d 📗 %s %s" total success pending failed
 
-  type t = {
-    opam : status_count;
-    versions : status_count;
-    universes : status_count;
-  }
+  type t = { opam : status_count; versions : status_count; universes : status_count }
 
   let empty =
     let empty_status () = { total = 0; failed = 0; pending = 0; success = 0 } in
-    {
-      opam = empty_status ();
-      versions = empty_status ();
-      universes = empty_status ();
-    }
+    { opam = empty_status (); versions = empty_status (); universes = empty_status () }
 
   type kind = Opam | Version | Universe
 
@@ -51,13 +36,13 @@ module Stats = struct
     | `FAILED -> { sc with total = sc.total + 1; failed = sc.failed + 1 }
     | `PENDING -> { sc with total = sc.total + 1; pending = sc.pending + 1 }
     | `SUCCESS -> { sc with total = sc.total + 1; success = sc.success + 1 }
+    | _ -> sc
 
   let update stats kind status =
     match kind with
     | Opam -> { stats with opam = update_status stats.opam status }
     | Version -> { stats with versions = update_status stats.versions status }
-    | Universe ->
-        { stats with universes = update_status stats.universes status }
+    | Universe -> { stats with universes = update_status stats.universes status }
 end
 
 type t = {
@@ -71,75 +56,82 @@ type error = [ `Not_found ]
 
 open Lwt.Syntax
 
-let get_cohttp ~query ~api () =
-  let body = `Assoc [ ("query", `String query#query); ("variables", `Null) ] in
+module type S = sig
+  type t
+
+  module Raw : sig
+    type t
+  end
+
+  val query : string
+
+  val unsafe_fromJson : Yojson.Basic.t -> Raw.t
+
+  val parse : Raw.t -> t
+end
+
+let get_cohttp (type a) (module Api : S with type t = a) ~api () =
+  let body = `Assoc [ ("query", `String Api.query); ("variables", `Null) ] in
   let serialized_body = Yojson.Basic.to_string body in
-  let headers =
-    Cohttp.Header.of_list [ ("Content-Type", "application/json") ]
-  in
-  let* response, body =
-    Cohttp_lwt_unix.Client.post ~headers ~body:(`String serialized_body) api
-  in
+  let headers = Cohttp.Header.of_list [ ("Content-Type", "application/json") ] in
+  let* response, body = Cohttp_lwt_unix.Client.post ~headers ~body:(`String serialized_body) api in
   let* body = Cohttp_lwt.Body.to_string body in
   match Cohttp.Code.(code_of_status response.status |> is_success) with
-  | false ->
-      Lwt.fail
-        (Failure ("Status: " ^ Cohttp.Code.string_of_status response.status))
+  | false -> Lwt.fail (Failure ("Status: " ^ Cohttp.Code.string_of_status response.status))
   | true ->
       let json = Yojson.Basic.(from_string body |> Util.member "data") in
-      Lwt.return (query#parse json)
-
-let last_update_query = Docs_api.Last_update.make ()
+      Lwt.return (Api.unsafe_fromJson json |> Api.parse)
 
 let get_last_update ~api () =
-  let+ res = get_cohttp ~query:last_update_query ~api () in
-  res#last_update
+  let+ res = get_cohttp (module Docs_api.Last_update) ~api () in
+  res.last_update
 
-let packages_query = Docs_api.Packages.make ()
-
-let get_packages = get_cohttp ~query:packages_query
+let get_packages = get_cohttp (module Docs_api.Packages)
 
 let parse_status t =
-  match t#status with
+  let open Docs_api.Packages in
+  match t.status with
   | `FAILED -> Package.Failed
   | `PENDING -> Pending
-  | `SUCCESS -> Built (t#blessed_universe |> Option.get)
+  | `SUCCESS -> Built (t.blessed_universe |> Option.get)
+  | `FutureAddedValue _ -> failwith "Unsupported value"
 
 let parse_version ~stats version =
+  let open Docs_api.Packages in
   let () =
-    stats := Stats.update !stats Version version#status;
-    version#universes
-    |> Array.iter (fun universe ->
-           stats := Stats.update !stats Universe universe#status)
+    stats := Stats.update !stats Version version.status;
+    version.universes
+    |> Array.iter (fun (universe : t_packages_versions_universes) ->
+           stats := Stats.update !stats Universe universe.status)
   in
   ( OpamPackage.create
-      (OpamPackage.Name.of_string version#name)
-      (OpamPackage.Version.of_string version#version),
+      (OpamPackage.Name.of_string version.name)
+      (OpamPackage.Version.of_string version.version),
     parse_status version )
 
 let parse_package ~stats package =
+  let open Docs_api.Packages in
   let () =
     let last_version =
-      package#versions
+      package.versions
       |> Array.fold_left
            (fun max_s s ->
              match max_s with
              | None -> Some s
-             | Some s' when s#version > s'#version -> Some s
+             | Some s' when s.version > s'.version -> Some s
              | Some s' -> Some s')
            None
       |> Option.get
     in
-    stats := Stats.update !stats Opam last_version#status
+    stats := Stats.update !stats Opam last_version.status
   in
-  package#versions |> Array.to_seq |> Seq.map (parse_version ~stats)
+  package.versions |> Array.to_seq |> Seq.map (parse_version ~stats)
 
 let parse data =
+  let open Docs_api.Packages in
   let stats = ref Stats.empty in
   let data =
-    data#packages |> Array.to_seq
-    |> Seq.flat_map (parse_package ~stats)
-    |> OpamPackage.Map.of_seq
+    data.packages |> Array.to_seq |> Seq.flat_map (parse_package ~stats) |> OpamPackage.Map.of_seq
   in
   (data, !stats)
 
@@ -158,7 +150,7 @@ let make ~api ~polling () =
     let packages, stats = parse data in
     res.packages <- packages;
     res.stats <- stats;
-    res.static_files_endpoint <- data#static_files_endpoint
+    res.static_files_endpoint <- data.static_files_endpoint
   in
   let last_update = ref "" in
   let rec updater () =
@@ -172,8 +164,7 @@ let make ~api ~polling () =
           else Lwt.return_unit)
         (fun exn ->
           Dream.error (fun log ->
-              log "Failed to contact the graphql api: %s"
-                (Printexc.to_string exn));
+              log "Failed to contact the graphql api: %s" (Printexc.to_string exn));
           Lwt.return ())
     in
     let* () = Lwt_unix.sleep (float_of_int polling) in
@@ -192,17 +183,11 @@ let extract_docs_body html =
   let open Soup in
   let soup = parse html in
   let preamble =
-    soup
-    |> select_one ".odoc-preamble"
-    |> Option.map to_string |> Option.value ~default:""
+    soup |> select_one ".odoc-preamble" |> Option.map to_string |> Option.value ~default:""
   in
-  let toc =
-    soup |> select_one ".odoc-toc" |> Option.map to_string
-    |> Option.value ~default:""
-  in
+  let toc = soup |> select_one ".odoc-toc" |> Option.map to_string |> Option.value ~default:"" in
   let content =
-    soup |> select_one ".odoc-content" |> Option.map to_string
-    |> Option.value ~default:""
+    soup |> select_one ".odoc-content" |> Option.map to_string |> Option.value ~default:""
   in
   toc ^ preamble ^ content
 
@@ -228,7 +213,6 @@ let load t path =
       let* load = try_load ~t (path ^ "/index.html") in
       match load with
       | Ok v -> Lwt.return_ok v
-      | Error _ ->
-          try_load ~t (Astring.String.trim ~drop:(( = ) '/') path ^ ".html")
+      | Error _ -> try_load ~t (Astring.String.trim ~drop:(( = ) '/') path ^ ".html")
   in
   res ()
