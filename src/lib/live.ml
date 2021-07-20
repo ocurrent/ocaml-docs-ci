ee624fd22883912cec7f49d740d01545fa981040
Lucas Pluvinage <lucas@tarides.com>
2021-07-20 12:07:27 +0200

    add timestamp in logs

--- src/lib/live.ml
+++ src/lib/live.ml
@@ -25,7 +25,8 @@ module Op = struct
     let storage_folder = Fpath.(of_string (Ssh.storage_folder ssh) |> Result.get_ok) in
     let target_folder = Fpath.(storage_folder // new_generation_folder) in
     let live_folder = Fpath.(storage_folder / (name ^ "-live")) in
-    let live_file = Fpath.add_ext "history" live_folder in
+    let live_file = Fpath.add_ext "log" live_folder in
+    let date_format = {|+"%Y-%m-%d %T"|} in
     let command =
       Bos.Cmd.(
         v "ssh" % "-p"
@@ -33,8 +34,8 @@ module Op = struct
         % "-i"
         % p (Ssh.priv_key_file ssh)
         % (Ssh.user ssh ^ "@" ^ Ssh.host ssh)
-        % Fmt.str "ln -sfT %a %a && echo '%a' >> %a" Fpath.pp target_folder Fpath.pp live_folder
-            Fpath.pp new_generation_folder Fpath.pp live_file)
+        % Fmt.str "ln -sfT %a %a && echo `date %s` '%a' >> %a" Fpath.pp target_folder Fpath.pp
+            live_folder date_format Fpath.pp new_generation_folder Fpath.pp live_file)
     in
     Current.Process.exec ~cancellable:true ~job ("", Bos.Cmd.to_list command |> Array.of_list)
 end
