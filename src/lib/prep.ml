29871cad2ac476e3659cc4fb1f59b56583e65ebf
Lucas Pluvinage <lucas@tarides.com>
2021-07-20 11:01:15 +0200

    Error path: tar in all cases

--- src/lib/prep.ml
+++ src/lib/prep.ml
@@ -77,7 +77,7 @@ let spec ~ssh ~voodoo ~base ~(install : Package.t) (prep : Package.t list) =
 
   let create_dir_and_copy_logs_if_not_exist =
     let command =
-      Fmt.str "([ -d $1 ] && %s) || (echo \"FAILED:$2\" && mkdir -p $1 && cp ~/opam.err.log $1)"
+      Fmt.str "([ -d $1 ] || (echo \"FAILED:$2\" && mkdir -p $1 && cp ~/opam.err.log $1)) && (%s)"
         (Misc.tar_cmd (Fpath.v "$1"))
     in
     Storage.for_all prep_storage_folders command
