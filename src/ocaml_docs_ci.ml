0be03f3aefe2c7456d6b71a0f5358bb8e570b2d8
David Allsopp <david.allsopp@metastack.com>
2022-03-30 15:00:26 +0100

    Update for Cmdliner 1.1.0 interface

--- src/ocaml_docs_ci.ml
+++ src/ocaml_docs_ci.ml
@@ -53,9 +53,8 @@ open Cmdliner
 
 let cmd =
   let doc = "an OCurrent pipeline" in
-  ( Term.(
+  Cmd.v (Cmd.info program_name ~doc) Term.(
       const main $ Current.Config.cmdliner $ Current_github.Auth.cmdliner $ Current_web.cmdliner
-      $ Docs_ci_lib.Config.cmdliner),
-    Term.info program_name ~doc )
+      $ Docs_ci_lib.Config.cmdliner)
 
-let () = Term.(exit @@ eval cmd)
+let () = exit @@ Cmd.eval cmd
