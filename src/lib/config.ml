5c5f793f5acac7e9bc8246ea04b238cfb32b8aba
sabine <sabine@users.noreply.github.com>
2023-02-09 17:16:07 +0100

    upgrade to odoc 2.2.0 (#95)
    Co-authored-by: Sabine Schmaltz <sabine@tarides.com>

--- src/lib/config.ml
+++ src/lib/config.ml
@@ -164,8 +164,8 @@ let v cap_file jobs track_packages take_n_last_versions ssh =
 let cmdliner =
   Term.(const v $ cap_file $ jobs $ track_packages $ take_n_last_versions $ Ssh.cmdliner)
 
-(* odoc pinned to tag 2.1.1 *)
-let odoc _ = "https://github.com/ocaml/odoc.git#f556f10aa67f80e83d1e3e66c4b478e8efe4e18d"
+(* odoc pinned to tag 2.2.0 *)
+let odoc _ = "https://github.com/ocaml/odoc.git#103dac4c370aa2ad5aca7ba54f02f8e06adb941b"
 let pool _ = "linux-x86_64"
 let jobs t = t.jobs
 let track_packages t = t.track_packages
