1c4fbf3660bc8330588eb8daed4845ff64621aeb
Navin Keswani <navin@novemberkilo.io>
2023-05-25 11:56:16 +1000

    Sketching out the status subcmd.

--- src/cli/README.md
+++ src/cli/README.md
@@ -6,17 +6,21 @@
 
 -- Notes
 
-`ocaml-docs-ci status` shows a dashboard of documentation build results across opam-repository packages. Packages can be filtered by maintainer substrings or tag names in the opam package description.
+`ocaml-docs-ci` default command brings up help
 
-`ocaml-docs-ci status <project_name>` show the build status of all versions of a project.
+`ocaml-docs-ci list --ci-cap <path to cap file> --project <project-name-infix>` shows a list of project names that have the given string as an infix
 
-`ocaml-docs-ci status <project_name> <version>` show the build status of a single version of a project.
+`ocaml-docs-ci status --ci-cap <path to cap file>` shows a dashboard of documentation build results across opam-repository packages. Packages can be filtered by maintainer substrings or tag names in the opam package description.
 
-`ocaml-docs-ci status <project_name> <version> jobs` show the build status of a single job
+`ocaml-docs-ci status --ci-cap <path to cap file> --project <project_name>` show the build status of all versions of a project.
 
-`ocaml-docs-ci logs <job-id>` display logs for an individual job
+`ocaml-docs-ci status --ci-cap <path to cap file> --project <project_name> --version <version>` show the build status of a single version of a project.
 
-`ocaml-docs-ci rebuild <job-id>` rebuild a specific job
+`ocaml-docs-ci status --ci-cap <path to cap file> --project <project_name> --version <version> --job <job-id>` show the build status of a single job
+
+`ocaml-docs-ci logs --ci-cap <path to cap file> --job <job-id>` display logs for an individual job
+
+`ocaml-docs-ci rebuild --ci-cap <path to cap file> --job <job-id>` rebuild a specific job
 
 ## Reference
 
--- src/cli/main.ml
+++ src/cli/main.ml
@@ -97,31 +97,90 @@ let cap =
   @@ Arg.opt Arg.(some Capnp_rpc_unix.sturdy_uri) None
   @@ Arg.info ~doc:"The ocaml-docs-ci.cap file." ~docv:"CAP" [ "ci-cap" ]
 
+(* fixed position argument *)
 let project_name =
   Arg.value
   @@ Arg.opt Arg.(some string) None
   @@ Arg.info ~doc:"The Opam Project name." ~docv:"PROJECT" [ "project"; "p" ]
 
+(* optional argument *)
 let project_version =
   Arg.value
   @@ Arg.opt Arg.(some string) None
   @@ Arg.info ~doc:"The Opam Project version." ~docv:"VERSION"
        [ "version"; "n" ]
 
-let cmd =
-  let doc = "Client for ocaml-docs-ci" in
-  let main () ci_uri project_name project_version =
-    match Lwt_main.run (main ~ci_uri ~project_name ~project_version) with
-    | Ok () -> ()
-    | Error (`Capnp ex) ->
-        Fmt.epr "%a@." Capnp_rpc.Error.pp ex;
-        exit 1
-    | Error (`Msg m) ->
-        Fmt.epr "%s@." m;
-        exit 1
-  in
-  let info = Cmd.info "ocaml-docs-ci" ~doc in
-  Cmd.v info
-    Term.(const main $ setup_log $ cap $ project_name $ project_version)
+(* let cmd =
+   let doc = "Client for ocaml-docs-ci" in
+   let main () ci_uri project_name project_version =
+     match Lwt_main.run (main ~ci_uri ~project_name ~project_version) with
+     | Ok () -> ()
+     | Error (`Capnp ex) ->
+         Fmt.epr "%a@." Capnp_rpc.Error.pp ex;
+         exit 1
+     | Error (`Msg m) ->
+         Fmt.epr "%s@." m;
+         exit 1
+   in
+   let info = Cmd.info "ocaml-docs-ci" ~doc in
+   Cmd.v info
+     Term.(const main $ setup_log $ cap $ project_name $ project_version) *)
 
-let () = exit @@ Cmd.eval cmd
+type statuscmd_conf = {
+  cap : Uri.t option;
+  project_name : string option;
+  project_version : string option;
+}
+
+type cmd_conf = Status of statuscmd_conf
+
+let run cmd_conf =
+  match cmd_conf with
+  | Status statuscmd_conf ->
+      let main () ci_uri project_name project_version =
+        match Lwt_main.run (main ~ci_uri ~project_name ~project_version) with
+        | Ok () -> ()
+        | Error (`Capnp ex) ->
+            Fmt.epr "%a@." Capnp_rpc.Error.pp ex;
+            exit 1
+        | Error (`Msg m) ->
+            Fmt.epr "%s@." m;
+            exit 1
+      in
+      main () statuscmd_conf.cap statuscmd_conf.project_name
+        statuscmd_conf.project_version
+
+let statuscmd_term run =
+  let combine () cap project_name project_version =
+    Status { cap; project_name; project_version } |> run
+  in
+  Term.(const combine $ setup_log $ cap $ project_name $ project_version)
+
+let statuscmd_doc = "[Some headline for status]"
+
+let statuscmd_man =
+  [ `S Manpage.s_description; `P "[multiline overview of statuscmd]" ]
+
+let statuscmd run =
+  let info = Cmd.info "status" ~doc:statuscmd_doc ~man:statuscmd_man in
+  Cmd.v info (statuscmd_term run)
+(*** Putting together the main command ***)
+
+let root_doc = "[some headline for the main command]"
+
+let root_man =
+  [ `S Manpage.s_description; `P "[multiline overview of the main command]" ]
+
+(*
+   Use the built-in action consisting of displaying the help page.
+*)
+(* let root_term = Term.ret (Term.const (`Help (`Pager, None))) *)
+let root_info = Cmd.info "ocaml-docs-ci-client" ~doc:root_doc ~man:root_man
+(* let root = Cmd.v root_info root_term *)
+let subcommands run = [ statuscmd run ]
+
+let parse_command_line_and_run (run : cmd_conf -> unit) =
+  Cmd.group root_info (subcommands run) |> Cmd.eval |> exit
+
+let main () = parse_command_line_and_run run
+let () = main ()
