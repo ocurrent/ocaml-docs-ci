1c647a53d314333f03212539c845b396131c0857
Mark Elvers <mark.elvers@tunbury.org>
2023-10-26 15:47:30 +0100

    Add percentage progress and --dry-run

--- src/cli/epoch.ml
+++ src/cli/epoch.ml
@@ -2,7 +2,27 @@ let ( / ) = Filename.concat
 
 module SS = Set.Make (String)
 
-let main base_dir =
+let remove ~root files =
+  let num = SS.cardinal files in
+  let () = Printf.printf "Deleting %i files\n" num in
+  let _ = SS.fold (fun del (i, l) ->
+    let pcent = Int.div (100 * i) num in
+    let nl = if pcent > l then
+        let () = Printf.printf "%i%%\r" pcent in
+        let () = flush stdout in pcent
+      else l in
+    let _ = Sys.command ("rm -rf " ^ (root / del)) in
+    (i + 1, nl)) files (0, 0) in
+  Printf.printf "Completed.\n"
+
+let print files =
+  let total = SS.fold (fun del i ->
+    let () = if i < 10 then Printf.printf "%s\n" del in
+    i + 1) files 0 in
+  if total >= 10 then
+    Printf.printf "... plus %i more\n" (total - 10)
+
+let main base_dir dry_run =
   let path = base_dir in
 
   let epochs =
@@ -19,37 +39,31 @@ let main base_dir =
 
   let epoch_files = List.fold_left (fun s epoch -> Array.fold_right SS.add (Sys.readdir epoch) s) SS.empty epochs in
 
-  (* Prep universes *)
-  let universes = path / "/prep/universes" in
-  let prep_files = Array.fold_right SS.add (Sys.readdir universes) SS.empty in
-  let debris = SS.diff prep_files epoch_files in
-  Printf.printf "Files to be deleted in prep: ";
-  SS.iter (fun file -> Printf.printf "%s " file) debris;
-  Printf.printf "\n";
-  debris |> SS.iter (fun del -> ignore @@ Sys.command ("rm -rf " ^ (universes / del)));
-
-  (* Compile universes. *)
-  let universes = path / "/compile/u" in
-  let comp_files = Array.fold_right SS.add (Sys.readdir universes) SS.empty in
-  let debris = SS.diff comp_files epoch_files in
-  Printf.printf "Files to be deleted in compile: ";
-  SS.iter (fun file -> Printf.printf "%s " file) debris;
-  Printf.printf "\n";
-  debris |> SS.iter (fun del -> ignore @@ Sys.command ("rm -rf " ^ (universes / del)))
+  List.iter
+    (fun universe ->
+      let universes = path / universe in
+      let univ_files = Array.fold_right SS.add (Sys.readdir universes) SS.empty in
+      let debris = SS.diff univ_files epoch_files in
+      let () = Printf.printf "Files to be deleted in %s:\n" universe in
+      let () = print debris in
+      if not dry_run then remove ~root:universes debris)
+    [ "prep/universes"; "compile/u" ]
 
 (* Command-line parsing *)
 
 open Cmdliner
 
 let base_dir =
-  Arg.(
-    required
-    @@ opt (some dir) None
-    @@ info ~docv:"BASE_DIR"
-         ~doc:
-           "Base directory containing epochs. eg \
-            /var/lib/docker/volumes/infra_docs-data/_data"
-         [ "base-dir" ])
+  Arg.(required
+       @@ opt (some dir) None
+       @@ info ~docv:"BASE_DIR"
+            ~doc: "Base directory containing epochs. eg /var/lib/docker/volumes/infra_docs-data/_data" ["base-dir"])
+
+let dry_run =
+  Arg.(value
+    @@ flag
+    @@ info ~docv:"DRY_RUN"
+         ~doc: "If set, only list the files to be deleted but do not deleted them" ["dry-run"])
 
 let version =
   match Build_info.V1.version () with
@@ -59,6 +73,7 @@ let version =
 let cmd =
   let doc = "Epoch pruning" in
   let info = Cmd.info "epoch" ~doc ~version in
-  Cmd.v info Term.(const main $ base_dir)
+  Cmd.v info
+    Term.(const main $ base_dir $ dry_run)
 
 let () = exit @@ Cmd.eval cmd
